e1842ace3e9775692e3a738d5ced672dd19bdf18
Bijay <bijay.pun333@gmail.com>
2024-06-01 13:49:25 +0545

    added: contact page added in view and urls are added and also templates folder and index.html added

--- toDoList/views.py
+++ toDoList/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render, HttpResponse
 def home(request):
     name = "John"
     return HttpResponse(f"<h1>Hello, {name}!</h1>")
+
+def contact(request):
+    phone = 87451100
+    return HttpResponse(f"<h1>Contact Us: {phone}</h1>")
